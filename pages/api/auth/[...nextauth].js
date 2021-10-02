5be977c8aee61de9ce889046559c05c2d8945cfe
Iykekelvins <kelvinochubili@gmail.com>
2021-10-02 19:24:23 +0100

    github signup setup

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,8 @@ import Providers from "next-auth/providers";
 export default NextAuth({
   providers: [
     Providers.GitHub({
-      clientId: "",
-      clientSecret: "",
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
 });
