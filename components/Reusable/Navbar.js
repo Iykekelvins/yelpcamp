f8f1723ebae6f27723c86f2e0ee5f16eca1aaa19
Iykekelvins <kelvinochubili@gmail.com>
2021-10-08 21:09:17 +0100

    sticky navbar created

--- components/Reusable/Navbar.js
+++ components/Reusable/Navbar.js
@@ -1,50 +1,105 @@
 import { Logo, Button } from "../index";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/client";
+import { appSvgs } from "../../public/appSvgs";
+import Image from "next/image";
+import { useState } from "react";
 
 const Navbar = () => {
   const [session, loading] = useSession();
-  console.log(session);
+  const [isNav, setIsNav] = useState(false);
   return (
-    <nav className="navbar flex-ac-jb">
-      <div className="navbar__left flex-ac">
-        <Logo />
-        <button className="nav-btn nav-btn__left">Home</button>
-      </div>
-      <div className="navbar__right flex-ac">
-        {!loading && !session && (
-          <Link href="/api/auth/signin">
-            <a
-              onClick={(e) => {
-                e.preventDefault();
-                signIn();
-              }}
-            >
-              <button className="nav-btn nav-btn__right">Login</button>
-            </a>
-          </Link>
-        )}
-        {session && (
-          <Link href="/api/auth/signout">
-            <a
-              onClick={(e) => {
-                e.preventDefault();
-                signOut();
-              }}
-            >
-              <button className="nav-btn">Logout</button>
-            </a>
-          </Link>
-        )}
-        {!session && !loading && (
-          <Link href="/sign-up">
-            <a>
-              <Button title="Create an account" />
-            </a>
-          </Link>
-        )}
-      </div>
-    </nav>
+    <header className="nav-header">
+      <nav className="navbar flex-ac-jb">
+        <div className="navbar__left flex-ac">
+          <Logo />
+          <button className="nav-btn nav-btn__left">Home</button>
+        </div>
+        <div className="navbar__right flex-ac">
+          {!loading && !session && (
+            <Link href="/api/auth/signin">
+              <a
+                onClick={(e) => {
+                  e.preventDefault();
+                  signIn();
+                }}
+              >
+                <button className="cta-btn nav-btn nav-btn__right">
+                  Login
+                </button>
+              </a>
+            </Link>
+          )}
+          {session && (
+            <Link href="/api/auth/signout">
+              <a
+                onClick={(e) => {
+                  e.preventDefault();
+                  signOut();
+                }}
+              >
+                <button className="cta-btn nav-btn">Logout</button>
+              </a>
+            </Link>
+          )}
+          {!session && !loading && (
+            <Link href="/sign-up">
+              <a className="cta-btn">
+                <Button title="Create an account" />
+              </a>
+            </Link>
+          )}
+          <button
+            className="toggle-btn hamburger-btn"
+            onClick={() => setIsNav(!isNav)}
+          >
+            <Image
+              src={appSvgs.hamburger}
+              height={24}
+              width={24}
+              alt="hamburger btn"
+            />
+          </button>
+        </div>
+      </nav>
+
+      {/* Phone Nav */}
+      {isNav && (
+        <nav className="phone-nav flex-ac-jc">
+          {!loading && !session && (
+            <Link href="/api/auth/signin">
+              <a
+                onClick={(e) => {
+                  e.preventDefault();
+                  signIn();
+                }}
+              >
+                <button className=" nav-btn nav-btn__right">Login</button>
+              </a>
+            </Link>
+          )}
+          {session && (
+            <Link href="/api/auth/signout">
+              <a
+                onClick={(e) => {
+                  e.preventDefault();
+                  signOut();
+                }}
+              >
+                <button className=" nav-btn">Logout</button>
+              </a>
+            </Link>
+          )}
+          {!session && !loading && (
+            <Link href="/sign-up">
+              <a className="">
+                <Button title="Create an account" />
+              </a>
+            </Link>
+          )}
+        </nav>
+      )}
+    </header>
   );
 };
 
