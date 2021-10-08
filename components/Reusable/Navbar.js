6907e0bbba8a0ad7a68ecc7873fb317da63e791e
Iykekelvins <kelvinochubili@gmail.com>
2021-10-08 22:01:08 +0100

    added session name for logged in users

--- components/Reusable/Navbar.js
+++ components/Reusable/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
       <nav className="navbar flex-ac-jb">
         <div className="navbar__left flex-ac">
           <Logo />
-          <button className="nav-btn nav-btn__left">Home</button>
+          <button className="cta-btn nav-btn nav-btn__left">Home</button>
         </div>
         <div className="navbar__right flex-ac">
           {!loading && !session && (
@@ -31,16 +31,21 @@ const Navbar = () => {
             </Link>
           )}
           {session && (
-            <Link href="/api/auth/signout">
-              <a
-                onClick={(e) => {
-                  e.preventDefault();
-                  signOut();
-                }}
-              >
-                <button className="cta-btn nav-btn">Logout</button>
-              </a>
-            </Link>
+            <div className="flex-ac">
+              <p className="name cta-btn">
+                {session.user.name || session.user.email}
+              </p>
+              <Link href="/api/auth/signout">
+                <a
+                  onClick={(e) => {
+                    e.preventDefault();
+                    signOut();
+                  }}
+                >
+                  <button className="cta-btn nav-btn">Logout</button>
+                </a>
+              </Link>
+            </div>
           )}
           {!session && !loading && (
             <Link href="/sign-up">
@@ -79,16 +84,19 @@ const Navbar = () => {
             </Link>
           )}
           {session && (
-            <Link href="/api/auth/signout">
-              <a
-                onClick={(e) => {
-                  e.preventDefault();
-                  signOut();
-                }}
-              >
-                <button className=" nav-btn">Logout</button>
-              </a>
-            </Link>
+            <>
+              <p className="name">{session.user.name || session.user.email}</p>
+              <Link href="/api/auth/signout">
+                <a
+                  onClick={(e) => {
+                    e.preventDefault();
+                    signOut();
+                  }}
+                >
+                  <button className=" nav-btn">Logout</button>
+                </a>
+              </Link>
+            </>
           )}
           {!session && !loading && (
             <Link href="/sign-up">
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import "../styles/index.scss";
 import Layout from "../components/Reusable/Layout";
 import { AppProvider } from "../context/context";
+import { Provider } from "next-auth/client";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AppProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </AppProvider>
+    <Provider>
+      <AppProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AppProvider>
+    </Provider>
   );
 }
 
